ac2d53f3235e5cf47618717fb81742ba8a8d50cf
AnnMarie Faust <amn5336@gmail.com>
2017-09-01 16:45:32 -0500

    Configure JMapFrame

---
+++ src/main/java/org/geotools/tutorial/crs/CRSLab.java
@@ -0,0 +1,95 @@
+package org.geotools.tutorial.crs;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JToolBar;
+import javax.swing.SwingWorker;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFactorySpi;
+import org.geotools.data.DefaultTransaction;
+import org.geotools.data.FeatureWriter;
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.Query;
+import org.geotools.data.Transaction;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.referencing.CRS;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.JProgressWindow;
+import org.geotools.swing.action.SafeAction;
+import org.geotools.swing.data.JFileDataStoreChooser;
+import org.opengis.feature.Feature;
+import org.opengis.feature.FeatureVisitor;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.util.ProgressListener;
+
+/**
+ * This is a visual example of changing the coordinate reference system of a feature layer.
+ */
+public class CRSLab {
+
+    private File sourceFile;
+    private SimpleFeatureSource featureSource;
+    private MapContent map;
+
+    public static void main(String[] args) throws Exception {
+        CRSLab lab = new CRSLab();
+        lab.displayShapefile();
+    }
+
+    private void displayShapefile() throws Exception {
+        sourceFile = JFileDataStoreChooser.showOpenFile("shp", null);
+        if (sourceFile == null) {
+            return;
+        }
+
+        FileDataStore store = FileDataStoreFinder.getDataStore(sourceFile);
+        featureSource = store.getFeatureSource();
+
+        // Create a map context and add our shapefile to it
+        map = new MapContent();
+        Style style = SLD.createSimpleStyle(featureSource.getSchema());
+        Layer layer = new FeatureLayer(featureSource, style);
+        map.layers().add(layer);
+
+        // Create a JMapFrame with custom toolbar buttons
+        JMapFrame mapFrame = new JMapFrame(map);
+        mapFrame.enableToolBar(true);
+        mapFrame.enableStatusBar(true);
+
+        JToolBar toolbar = mapFrame.getToolBar();
+        toolbar.addSeparator();
+        toolbar.add(new JButton(new ValidateGeometryAction()));
+        toolbar.add(new JButton(new ExportShapeFileAction()));
+
+        // Display the map frame; when it is closed the application will exit
+        mapFrame.setSize(800, 600);
+        mapFrame.setVisible(true);
+    }
+
+}
