aa4cb979a03a6da1d965a415b8c86d708464de2e
AnnMarie Faust <amn5336@gmail.com>
2017-09-01 16:21:59 -0500

    Write feature data to shapefile

--- src/main/java/org/geotools/tutorial/feature/Csv2Shape.java
+++ src/main/java/org/geotools/tutorial/feature/Csv2Shape.java
@@ -50,7 +50,8 @@ public class Csv2Shape {
             return;
         }
 
-        /*
+        // docs break feature type
+        /**
          * We use the DataUtilities class to create a FeatureType that will describe the data in our
          * shapefile.
          *
@@ -63,15 +64,18 @@ public class Csv2Shape {
         );
         System.out.println("TYPE:"+TYPE);
 
-        /*
+        // docs break feature collection
+        /**
          * A list to collect features as we create them.
          */
+
         List<SimpleFeature> features = new ArrayList<>();
 
-        /*
+        /**
          * GeometryFactory will be used to create the geometry attribute of each feature,
          * using a Point object for the location.
          */
+
         GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
 
         SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(TYPE);
@@ -102,9 +106,11 @@ public class Csv2Shape {
             }
         }
 
-        /*
+        // docs break new shapefile
+        /**
          * Get an output file name and create the new shapefile
          */
+
         File newFile = getNewShapeFile(file);
 
         ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
@@ -115,12 +121,64 @@ public class Csv2Shape {
 
         ShapefileDataStore newDataStore = (ShapefileDataStore) dataStoreFactory.createNewDataStore(params);
 
-        /*
+        /**
          * TYPE is used as a template to describe the file contents
          */
+
         newDataStore.createSchema(TYPE);
 
+        // docs break transaction
+        /**
+         * Write the features to the shapefile
+         */
+
+        Transaction transaction = new DefaultTransaction("create");
+
+        String typeName = newDataStore.getTypeNames()[0];
+        SimpleFeatureSource featureSource = newDataStore.getFeatureSource(typeName);
+        SimpleFeatureType SHAPE_TYPE = featureSource.getSchema();
+
+        /**
+         * The Shapefile format has a couple limitations:
+         * - "the_geom" is always first, and used for the geometry attribute name
+         * - "the_geom" must be of type Point, MultiPoint, MuiltiLineString, MultiPolygon
+         * - Attribute names are limited in length
+         * - Not all data types are supported (example Timestamp represented as Date)
+         *
+         * Each data store has different limitations so check the resulting SimpleFeatureType.
+         */
+
+        System.out.println("SHAPE:"+SHAPE_TYPE);
+
+        if (featureSource instanceof SimpleFeatureStore) {
+            SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
+
+            /**
+             * SimpleFeatureStore has a method to add features from a
+             * SimpleFeatureCollection object, so we use the ListFeatureCollection
+             * class to wrap our list of features.
+             */
+
+            SimpleFeatureCollection collection = new ListFeatureCollection(TYPE, features);
+            featureStore.setTransaction(transaction);
+            try {
+                featureStore.addFeatures(collection);
+                transaction.commit();
+            } catch (Exception problem) {
+                problem.printStackTrace();
+                transaction.rollback();
+            } finally {
+                transaction.close();
+            }
+            System.exit(0); // success!
+        } else {
+            System.out.println(typeName + " does not support read/write access");
+            System.exit(1);
+        }
+    }
+
+    // end main
 
     }
-}
+
 
